7b6c724cda2f1a07f668408799d5f3dc66103910
Campbell Hutcheson <nchutcheson@gmail.com>
2023-05-20 13:52:02 -0700

    updated claim prompt

--- cito/prompts.py
+++ cito/prompts.py
@@ -8,7 +8,7 @@ from langchain.prompts.chat import (
 claims_prompt_template = """
 Argument: {argument}
 
-Given the argument above. Break the argument into its claims.
+Given the argument above. Break the argument into its claims. Each claim should be independent and should contain all information necessary to understand the claim.
 
 Return the claims as a JSON list.
 
@@ -16,7 +16,7 @@ Return the claims as a JSON list.
 
 Example argument: Argument: The uprising in Libya started a wave of social protest. These social protests are dangerous to the United States and its interests. Hillary Clinton wrote an email to George Bush detailing these concerns in 2007.
 
-Example response: ["The uprising in Libya started a wave of social protest.", "These social protests are dangerous to the United States and its interests.", "Hillary Clinton wrote an email to George Bush detailing these concerns in 2007."]
+Example response: ["The uprising in Libya started a wave of social protest.", "The social protests in Libya are dangerous to the United States and its interests.", "Hillary Clinton wrote an email to George Bush detailing these concerns in 2007."]
 """
 
 claims_prompt = HumanMessagePromptTemplate.from_template(claims_prompt_template)
