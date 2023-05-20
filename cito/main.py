87ba51106f5ae27fe28b7476f7230fdd659ee3f5
Campbell Hutcheson <nchutcheson@gmail.com>
2023-05-20 14:59:05 -0700

    fixed :

--- cito/main.py
+++ cito/main.py
@@ -1,7 +1,7 @@
 from cito.cite import cite
 from cito.format import format_citations
 
-citations = cite("~/Projects/legal-hackathon/test_argument.txt"):
+citations = cite("~/Projects/legal-hackathon/test_argument.txt")
 
 print(format_citations(citations))
 
