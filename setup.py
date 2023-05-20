d7e2e99f59c828216d8b3d134d62334723e7d5c6
Campbell Hutcheson <nchutcheson@gmail.com>
2023-05-20 09:19:34 -0700

    module

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="",
+    version="0.1",
+    description="cito",
+    author="Hutcheson et al.",
+    author_email="nchutcheson@gmail.com",
+    packages=["cito"],
+    install_requires=[
+        "langchain",
+        "openai",
+    ],  
+)
